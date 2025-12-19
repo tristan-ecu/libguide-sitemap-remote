57abd82e03d1d8583c5884ee7c4aa81401121586
tristan-ecu <t.lyons@ecu.edu.au>
2025-12-19 14:47:11 +0800

    pagefind fix

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.on('eleventy.after', () => {
-    execSync(`npx pagefind --source _site --glob \"**/*.html\"`, { encoding: 'utf-8' })
+    execSync(`npx pagefind --source _site --glob \"**/*.html\" --base-path /libguide-sitemap-remote/`, { encoding: 'utf-8' })
   });
 };
